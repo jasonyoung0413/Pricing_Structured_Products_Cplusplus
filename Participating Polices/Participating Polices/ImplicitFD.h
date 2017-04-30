850dffdbc754af14c6fe3829baf309e71dcf5e81
stan wang <stan.wang.hk@gmail.com>
2017-04-30 15:33:52 +0800

    stan

--- Participating Polices/Participating Polices/ImplicitFD.h
+++ Participating Polices/Participating Polices/ImplicitFD.h
@@ -6,6 +6,7 @@ public:
   ParticipatingPolices* product;
   int max_i,max_j;
   double delta_a, delta_p;
+  double delta_s = 1 / max_k;
   double** value, ** value_sur;
   ImplicitFD(ParticipatingPolices* p,int i,int j):
       product(p),max_i(i),max_j(j){
@@ -14,9 +15,7 @@ public:
   }
   void terPayoff();
   void backwardEval();
-  void delMem(double** p);
 private:
-  double** initVal();
   int a_bar = 1000, p_bar = 2000;
   // the number of time steps per year is fixed at 100.
   int max_k=100;
--- Participating Polices/Participating Polices/ImplicitFD_part1.cpp
+++ Participating Polices/Participating Polices/ImplicitFD_part1.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 double**
-ImplicitFD::initVal(){
+initVal_2d(int max_j,int max_i){
   double **p;
-  p = new double* [max_i+1];
-  for (int i = 0; i <= max_i; i++) {
-	  p[i] = new double[max_j + 1];
+  p = new double* [max_j+1];
+  for (int j = 0; j <= max_j; j++) {
+	  p[j] = new double[max_i + 1];
   }
   return p;
 }
 
 void
-ImplicitFD::delMem(double** p){
-  for (int i=0;i<=max_i;i++){
+delMem_2d(double** p, int max_j){
+  for (int i=0;i<=max_j;i++){
     delete[] p[i];
   }
   delete[]p;
@@ -35,7 +35,7 @@ ImplicitFD::backwardEval() {
 			// PSOR
 
 			// surrender value
-			product->surrender_value[s - 1];
+			product->surrender_value[s - 1] = value_sur[0][0] - value[0][0];
 		}
 
     }
@@ -43,10 +43,37 @@ ImplicitFD::backwardEval() {
 	for (int k = 1; k <= max_k; k++) {
       // at the kth time step of year s.
       last_value = value;
-	  for (int i = 0; i <= max_i; i++) {
-		  value[i] = 
+	  for (int j = 0; j <= max_j; j++) {
+		  value = initVal_2d(max_j, max_i);
+		  value[j][0] = (1 - product->interest_rate*delta_s)*last_value[j][0];
+		  // matrix of coefficient
+		  double** coef;
+		  coef = initVal_2d(max_i - 1, 4);
+		  for (int i = 1; i < max_i; i++ ) {
+			  double temp = pow(product->sigma*i, 2);
+			  coef[i][1] = (product->interest_rate*i - temp)*delta_s / 2;
+			  coef[i][2] = 1 + delta_s*(product->interest_rate + temp);
+			  coef[i][3] = (- product->interest_rate*i - temp)*delta_s / 2;
+			  coef[i][4] = last_value[j][i];
+			  if (i == 1) {
+				  coef[i][4] -= coef[i][1] * (1 - product->interest_rate*delta_s)*last_value[j][0];
+				  coef[i][1] = 0;
+			  }
+			  if (i == max_i - 1) {
+				 coef[i][3] += 2 * coef[i][2];
+				 coef[i][2] = coef[i][1] - coef[i][2];
+				 coef[i][1] = 0;
+			  }
+		  }
+		  value[j] = Thomas(last_value[j]);
+		  value[j][max_i] = 2 * value[j][max_i - 1] - value[j][max_i - 2];
 	  }
-      delMem(last_value);
+	  delMem_2d(last_value, max_j);
     }
   }
+}
+
+double *
+Thomas(double* last_v) {
+
 }
